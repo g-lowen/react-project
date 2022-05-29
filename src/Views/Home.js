220cb8ec056bd45db91c94389a7cd7bc7ffe0cfc
Gustav Löwen-Åberg <gustav_lowen@hotmail.com>
2022-05-29 18:21:22 +0200

    Unfunctional addplayer function

--- src/Views/Home.js
+++ src/Views/Home.js
@@ -1,41 +1,45 @@
-import './Home.css'
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import "./Home.css";
+import axios from "axios";
+import { Formik } from "formik";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 function Home() {
-  const [AddPlayer, setAddPlayer] = useState('')
-  const [PlayerName, setPlayerName] = useState('')
-  const [value, setValue] = useState(null)
-  const [total, setTotal] = useState(null)
-  const [char, setChar] = useState(null)
-  // console.log(char)
-  let sum = []
+  const [AddPlayer, setAddPlayer] = useState("");
+  const [PlayerName, setPlayerName] = useState("");
+  const [PlayerTables, setPlayerTables] = useState([]);
+  const [value, setValue] = useState(null);
+  const [total, setTotal] = useState(null);
+  const [char, setChar] = useState(null);
+  const H1 = styled.h1`
+    text-align: center;
+  `;
+  let sum = [];
 
-  const TenRows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  const TenRows = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const PlayerTable = (
     <table>
       <thead>
         <tr>
           <th>
             {char === null ? (
-              'Icon'
+              "Icon"
             ) : (
               <img className="table-icon" src={char.image} alt="Character" />
             )}
           </th>
-          <th>{PlayerName === '' ? 'char.name' : PlayerName}</th>
+          <th>
+            {/* {char === null ? "Name" : char.name} */}
+            {PlayerName}
+          </th>
         </tr>
       </thead>
       <tbody>
         {TenRows.map((i) => (
           <tr key={i}>
             <td>{i}</td>
-            <td className="test-container">
-              <input
-                className="test"
-                type="number"
-                onBlur={handleValue}
-              ></input>
+            <td className="row-container">
+              <input className="row" type="number" onBlur={handleValue}></input>
             </td>
           </tr>
         ))}
@@ -50,102 +54,118 @@ function Home() {
         </tr>
       </tfoot>
     </table>
-  )
-  let PlayerTables = []
-  PlayerTables.push(PlayerTable)
-  PlayerTables.push(PlayerTable)
-  PlayerTables.push(PlayerTable)
-  // PlayerTables.push(PlayerTable)
-  // PlayerTables.push(PlayerTable)
-  // PlayerTables.push(PlayerTable)
-  // PlayerTables.push(PlayerTable)
-  // PlayerTables.push(PlayerTable)
-  // PlayerTables.push(PlayerTable)
-  // console.log(PlayerTables.length)
-  // console.log(PlayerTables)
+  );
+  // let PlayerTables = [];
+  // PlayerTables.push(PlayerTable);
   function handleChange(event) {
-    // console.log(event.target.value);
-    setAddPlayer(event.target.value)
+    console.log(event.target.value);
+    console.log(PlayerTables.length);
+    console.log(PlayerTables);
+    setAddPlayer(event.target.value);
   }
-
+  console.log(PlayerTables.length);
+  console.log(PlayerTables);
   function handleSubmit(event) {
-    setPlayerName(AddPlayer)
-    PlayerTables.push(PlayerTable)
-    // console.log(PlayerTables.length)
-    // console.log(PlayerTables)
-    setTotal(Number(2))
-    fetchData()
-
-    console.log(total)
-    event.preventDefault()
+    console.log(AddPlayer);
+    setPlayerName(AddPlayer);
+    PlayerTables.push(PlayerTable);
+    // setPlayerTables(PlayerTable);
+    // console.log(totallyValue);
+    // setTotal(Number(totallyValue));
+    // fetchData();
+    // console.log(total);
+    event.preventDefault();
   }
   function handleValue(event) {
-    // console.log(event.target.value)
-    let obj = JSON.parse(JSON.stringify(event.target.value))
-    sum.push(obj)
-    sum = [...sum]
-    console.log(sum)
-    totallyValue()
+    let obj = JSON.parse(JSON.stringify(event.target.value));
+    sum.push(obj);
+    sum = [...sum];
+    console.log(sum);
+    totallyValue();
     // setValue(totallyValue)
     // console.log(obj)
     // console.log(sum)
   }
   function totallyValue() {
-    const copy = sum
-    let totallySum = 0
+    const copy = sum;
+    let totallySum = 0;
     for (let i = 0; i < copy.length; i++) {
-      totallySum += Number(copy[i])
+      totallySum += Number(copy[i]);
     }
-    console.log(totallySum)
-    // setTotal(totallyValue)
+    console.log(totallySum);
   }
   // useEffect(() => {
-  function fetchData() {
-    axios.get('http://hp-api.herokuapp.com/api/characters').then((res) => {
-      let result = res.data
-      // console.log(result)
-      // console.log(result[0].image)
-      function filterByImage(result) {
-        if (result.image !== '') {
-          return true
-        }
-      }
-      let resultByImage = result.filter(filterByImage),
-        rand = Math.floor(Math.random() * resultByImage.length),
-        randChar = resultByImage[rand]
-      // console.log(randChar)
-      setChar(randChar)
-    })
-  }
+  // function fetchData() {
+  //   axios.get("http://hp-api.herokuapp.com/api/characters").then((res) => {
+  //     let result = res.data;
+  //     // console.log(result)
+  //     // console.log(result[0].image)
+  //     function filterByImage(result) {
+  //       if (result.image !== "") {
+  //         return true;
+  //       }
+  //     }
+  //     let resultByImage = result.filter(filterByImage),
+  //       rand = Math.floor(Math.random() * resultByImage.length),
+  //       randChar = resultByImage[rand];
+  //     console.log(randChar);
+  //     setChar(randChar);
+  //   });
+  // }
 
   // useEffect(fetchData, [])
   // fetchData()
   // }, [])
   return (
     <div className="Home">
-      <form className="addPlayer" onSubmit={handleSubmit}>
-        <div className="addPlayer-container">
-          <input
-            className="addPlayer-container-btn"
-            type="submit"
-            value="Add"
-          />
-          <input
-            className="addPlayer-container-input"
-            placeholder="Player name"
-            onChange={handleChange}
-          />
-        </div>
-      </form>
+      <Formik
+        initialValues={{ playerName: "" }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            setSubmitting(false);
+          }, 1000);
+        }}
+        validate={(values) => {
+          const errors = {};
+
+          if (values.userName.trim() === "") {
+            errors.userName = "Player name cannot be empty";
+          }
+
+          return errors;
+        }}
+      >
+        {({ handleChange, handleSubmit, isSubmitting, values }) => (
+          <form className="addPlayer" onSubmit={handleSubmit}>
+            <div className="addPlayer-container">
+              <input
+                className="addPlayer-container-btn"
+                disabled="isSubmitting"
+                type="submit"
+                value="Add"
+              />
+              <input
+                className="addPlayer-container-input"
+                name="playerName"
+                placeholder="Player name"
+                onChange={handleChange}
+              />
+            </div>
+          </form>
+        )}
+      </Formik>
+
       <div className="Home-player-container">
-        {/* <div>{char === null ? 'Icon' : char.name}</div>
-        <div>
-          {char === null ? 'Name' : <img src={char.image} alt="Character" />}
-        </div> */}
         {PlayerTables.length < 1 ? <div>Welcome to 10 000</div> : PlayerTables}
       </div>
+
+      {PlayerTables.length < 1 ? (
+        <H1>Welcome to 10 000</H1>
+      ) : (
+        <div className="Home-player-container"> {PlayerTables}</div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
